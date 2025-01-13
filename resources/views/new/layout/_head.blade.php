888ccbd2b1cf5f71e3c5e9707694b4cd6b53d566
maxhome <arshadvk7560@gmail.com>
2025-01-13 17:22:16 +0400

    commit

--- resources/views/new/layout/_head.blade.php
+++ resources/views/new/layout/_head.blade.php
@@ -46,6 +46,6 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <!-- <link href="https://fonts.googleapis.com/css2?family=Chilanka:wght@400;700&display=swap" rel="stylesheet" /> -->
+    <link href="https://fonts.googleapis.com/css2?family=Chilanka:wght@400;700&display=swap" rel="stylesheet" />
 
 </head>
