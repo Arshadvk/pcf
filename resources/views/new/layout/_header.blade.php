7d19f70f2e7714db58acd5c78a07987c90484f79
maxhome <arshadvk7560@gmail.com>
2025-06-27 17:44:02 +0400

    update

--- resources/views/new/layout/_header.blade.php
+++ resources/views/new/layout/_header.blade.php
@@ -83,7 +83,7 @@
                     <li><a href="/">Home</a></li>
                     <li><a href="/gallery">Gallery</a></li>
                     <li><a href="/about">About Us</a></li>
-                    <li><a href="/our-leaders">PDP Leaderss</a></li>
+                    <li><a href="/our-leaders">PDP Leaders</a></li>
                     <li><a href="/emirates">Emirates</a></li>
                     <li><a href="/news">News</a></li>
                     <li><a href="/contact">Contact</a></li>
